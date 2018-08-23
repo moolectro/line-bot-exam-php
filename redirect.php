b17e2912a94ccc396c339ba3115ddffc7b8d7921
moolectro <36756504+moolectro@users.noreply.github.com>
2018-08-23 09:47:21 +0700

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,1 +1,13 @@
-
+<?php 
+$json = file_get_contents
+  ('https://api.instagram.com/v1/tags/Thailand/media/recent?client_id=09aa1fde3b7f4540823356977a348893'); 
+$decode = json_decode($json, true); 
+?>
+<?php 
+    foreach ($decode['data'] as $data) { 
+       $thumbnail = $data['images']['low_resolution']['url']; 
+		?>
+		<div align="left"> 
+       	 <img src="<?php echo $thumbnail ?>" alt="" align="left" />
+		</div>
+<?php }?> 
