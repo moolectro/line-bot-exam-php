c86709ffdd7dbf8d093c155c80f04fd0e1c61b6b
moolectro <36756504+moolectro@users.noreply.github.com>
2018-08-23 09:53:38 +0700

    Update igbot.php

--- igbot.php
+++ igbot.php
@@ -1,13 +1,24 @@
-<?php 
-$json = file_get_contents
-  ('https://api.instagram.com/v1/tags/Thailand/media/recent?client_id=09aa1fde3b7f4540823356977a348893'); 
-$decode = json_decode($json, true); 
-?>
-<?php 
-    foreach ($decode['data'] as $data) { 
-       $thumbnail = $data['images']['low_resolution']['url']; 
-		?>
-		<div align="left"> 
-       	 <img src="<?php echo $thumbnail ?>" alt="" align="left" />
-		</div>
-<?php }?> 
+$access_token="1499419437.09aa1fd.0d00670bb5f8428d9e05ae9a3fd747af";
+$photo_count=10; // จำนวนรูปภาพที่ต้องการดึงมาแสดง
+     
+$json_url="https://api.instagram.com/v1/users/self/media/recent/?";
+$json_url.="access_token={$access_token}&count={$photo_count}";
+ 
+$json_data = file_get_contents($json_url);
+$obj = json_decode($json_data, true, 512, JSON_BIGINT_AS_STRING); // PHP 5.4 ขึ้นไป
+ 
+ 
+foreach ($obj['data'] as $post) {
+     
+    echo $post['caption']['text'];
+ 
+    echo $post['link'];
+ 
+    echo str_replace("http://", "https://", $post['images']['low_resolution']['url']); // url ของรูปภาพขนาดเล็กสุด
+ 
+   // ภาพมี 3 ขนาดคือ low_resolution, standard_resolution, thumbnail, 
+ 
+     
+    // ดู key,value ทั้งหมดจาก json นะครับ
+    // https://api.instagram.com/v1/users/self/media/recent/?access_token=1499419437.09aa1fd.0d00670bb5f8428d9e05ae9a3fd747af&count=1
+}
